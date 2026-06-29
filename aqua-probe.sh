a601b9e160d41045975427538bdc9d34d55c86d7
Eric Gomes <38416014+ericgomes56@users.noreply.github.com>
2026-06-29 12:44:51 -0400

    Apply namespace-admin-bad individually

--- aqua-probe.sh
+++ aqua-probe.sh
@@ -1805,7 +1805,7 @@ rules:
   resources: ["*"]
   verbs: ["*"]
 EOF
-            kubectl apply -f - <<EOF
+            apply_non_compliant_resource "namespace-admin-bad" <<EOF
 apiVersion: rbac.authorization.k8s.io/v1
 kind: Role
 metadata:
@@ -1815,7 +1815,8 @@ rules:
 - apiGroups: ["*"]
   resources: ["*"]
   verbs: ["*"]
----
+EOF
+            kubectl apply -f - <<EOF
 apiVersion: rbac.authorization.k8s.io/v1
 kind: Role
 metadata:
